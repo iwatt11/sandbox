a1fa0112db1f0351bc9cb8fda6737df1dc64ec3c
iwatt <ian.watt@roubini.com>
2015-02-11 13:55:09 -0500

    New images.

--- image_scrape.py
+++ image_scrape.py
@@ -3,7 +3,7 @@ import urllib
 import urllib2
 from bs4 import BeautifulSoup
 
-os.chdir('C:\Users\iwatt\PycharmProjects\Sandbox\Images')
+os.chdir('C:\Users\iwatt\PycharmProjects\Sandboxx\Images')
 url = "http://www.newyorker.com/humor"
 html = urllib2.urlopen(url)
 soup = BeautifulSoup(html)
@@ -12,6 +12,7 @@ imgs = soup.findAll("img", {"class": "owl-lazy lazyOwl landscape  cartoon"})
 for img in imgs:
     imgUrl = img['data-medium-src']
     imgName = os.path.basename(imgUrl)
+    print imgName
     directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), "Images")
     filename = os.path.join(directory, imgName)
     urllib.urlretrieve(imgUrl, filename)
